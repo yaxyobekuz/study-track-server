f5b6645bc226cc9a7b9450f210559f1ffa424bd8
yaxyobekuz <pubgn9642@gmail.com>
2025-12-26 16:57:17 +0500

    feat: add schedule model with validation and unique index

---
+++ src/models/schedule.model.js
@@ -0,0 +1,58 @@
+// Mongoose
+const mongoose = require("mongoose");
+
+const scheduleSchema = new mongoose.Schema(
+  {
+    class: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class",
+      required: [true, "Sinf majburiy"],
+    },
+    day: {
+      type: String,
+      required: [true, "Kun majburiy"],
+      enum: [
+        "dushanba",
+        "seshanba",
+        "chorshanba",
+        "payshanba",
+        "juma",
+        "shanba",
+      ],
+    },
+    subjects: [
+      {
+        subject: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Subject",
+          required: true,
+        },
+        teacher: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        order: {
+          type: Number,
+          required: true,
+        },
+        startTime: {
+          type: String,
+        },
+        endTime: {
+          type: String,
+        },
+      },
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+scheduleSchema.index({ class: 1, day: 1 }, { unique: true });
+
+module.exports = mongoose.model("Schedule", scheduleSchema);
