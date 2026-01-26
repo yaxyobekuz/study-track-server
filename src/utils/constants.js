ae25d773bef867fc9946c9dcea44f962488a1f1b
yaxyobekuz <pubgn9642@gmail.com>
2026-01-26 08:20:54 +0500

    feat: add Uzbek days of the week and grade boundaries constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,4 +15,32 @@ const DAYS = {
   SATURDAY: "shanba",
 };
 
-module.exports = { ROLES, DAYS };
+// Hafta kunlari massivi (o'zbek tilida)
+const DAYS_UZ = [
+  "yakshanba", // 0 - Sunday
+  "dushanba", // 1 - Monday
+  "seshanba", // 2 - Tuesday
+  "chorshanba", // 3 - Wednesday
+  "payshanba", // 4 - Thursday
+  "juma", // 5 - Friday
+  "shanba", // 6 - Saturday
+];
+
+// Baho chegaralari
+const GRADE_MIN = 2;
+const GRADE_MAX = 5;
+
+// Pagination default qiymatlari
+const PAGINATION_DEFAULTS = {
+  PAGE: 1,
+  LIMIT: 24,
+};
+
+module.exports = {
+  ROLES,
+  DAYS,
+  DAYS_UZ,
+  GRADE_MIN,
+  GRADE_MAX,
+  PAGINATION_DEFAULTS
+};
