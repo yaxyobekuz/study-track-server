089027881cbc8d5027dfadb84cd612253f1dd747
yaxyobekuz <pubgn9642@gmail.com>
2026-03-23 09:37:10 +0500

    feat: refactor class controller and routes to utilize class service and improve validation

--- src/controllers/class.controller.js
+++ src/controllers/class.controller.js
@@ -1,286 +1,106 @@
 const ExcelService = require("../services/excel.service");
-const Class = require("../models/class.model");
-const User = require("../models/user.model");
+const classService = require("../services/class.service");
+const asyncHandler = require("../middleware/async.middleware");
 
 // Get all classes
-const getAllClasses = async (req, res) => {
-  try {
-    const classes = await Class.find()
-      .populate("createdBy", "firstName lastName")
-      .sort({ name: 1 })
-      .lean();
+const getAllClasses = asyncHandler(async (req, res) => {
+  const classes = await classService.getAllClasses();
 
-    res.json({
-      success: true,
-      count: classes.length,
-      data: classes,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.json({
+    success: true,
+    data: classes,
+  });
+});
 
 // Get single class
-const getClass = async (req, res) => {
-  try {
-    const classData = await Class.findById(req.params.id).populate(
-      "createdBy",
-      "firstName lastName",
-    );
+const getClass = asyncHandler(async (req, res) => {
+  const data = await classService.getClassById(req.params.id);
 
-    if (!classData) {
-      return res.status(404).json({
-        success: false,
-        message: "Sinf topilmadi",
-      });
-    }
-
-    // Get class students
-    const students = await User.find({
-      classes: req.params.id,
-      role: "student",
-    })
-      .select("-password")
-      .sort({ lastName: 1, firstName: 1 });
-
-    res.json({
-      success: true,
-      data: {
-        ...classData.toObject(),
-        students,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.json({
+    success: true,
+    data,
+  });
+});
 
 // Create new class (Owner only)
-const createClass = async (req, res) => {
-  try {
-    const { name } = req.body;
+const createClass = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  const data = await classService.createClass(name, req.user._id);
 
-    if (!name) {
-      return res.status(400).json({
-        success: false,
-        message: "Sinf nomi majburiy",
-      });
-    }
-
-    const classData = await Class.create({
-      name,
-      createdBy: req.user._id,
-    });
-
-    const populatedClass = await Class.findById(classData._id).populate(
-      "createdBy",
-      "firstName lastName",
-    );
-
-    res.status(201).json({
-      success: true,
-      message: "Sinf muvaffaqiyatli yaratildi",
-      data: populatedClass,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.status(201).json({
+    success: true,
+    message: "Sinf muvaffaqiyatli yaratildi",
+    data,
+  });
+});
 
 // Update class (Owner only)
-const updateClass = async (req, res) => {
-  try {
-    const { name, isActive } = req.body;
+const updateClass = asyncHandler(async (req, res) => {
+  const data = await classService.updateClass(req.params.id, req.body);
 
-    const classData = await Class.findById(req.params.id);
-
-    if (!classData) {
-      return res.status(404).json({
-        success: false,
-        message: "Sinf topilmadi",
-      });
-    }
-
-    if (name) classData.name = name;
-    if (isActive !== undefined) classData.isActive = isActive;
-
-    await classData.save();
-
-    const updatedClass = await Class.findById(classData._id).populate(
-      "createdBy",
-      "firstName lastName",
-    );
-
-    res.json({
-      success: true,
-      message: "Sinf muvaffaqiyatli yangilandi",
-      data: updatedClass,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.json({
+    success: true,
+    message: "Sinf muvaffaqiyatli yangilandi",
+    data,
+  });
+});
 
 // Delete class (Owner only)
-const deleteClass = async (req, res) => {
-  try {
-    const classData = await Class.findById(req.params.id);
+const deleteClass = asyncHandler(async (req, res) => {
+  await classService.deleteClass(req.params.id);
 
-    if (!classData) {
-      return res.status(404).json({
-        success: false,
-        message: "Sinf topilmadi",
-      });
-    }
-
-    // Check if class has students
-    const studentsCount = await User.countDocuments({
-      classes: req.params.id,
-      role: "student",
-    });
-
-    if (studentsCount > 0) {
-      return res.status(400).json({
-        success: false,
-        message:
-          "Bu sinfda o'quvchilar bor. Avval o'quvchilarni boshqa sinfga o'tkazing",
-      });
-    }
-
-    await classData.deleteOne();
-
-    res.json({
-      success: true,
-      message: "Sinf muvaffaqiyatli o'chirildi",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.json({
+    success: true,
+    message: "Sinf muvaffaqiyatli o'chirildi",
+  });
+});
 
 // Export class students to Excel
-const exportClassStudents = async (req, res) => {
-  try {
-    const classData = await Class.findById(req.params.id);
+const exportClassStudents = asyncHandler(async (req, res) => {
+  const { classData, data } = await classService.getClassStudentsForExport(
+    req.params.id,
+  );
 
-    if (!classData) {
-      return res.status(404).json({
-        success: false,
-        message: "Sinf topilmadi",
-      });
-    }
+  const workbook = ExcelService.createExcel({
+    sheetName: classData.name,
+    columns: [
+      { header: "F.I.O", key: "fullName", width: 30 },
+      { header: "Username", key: "username", width: 20 },
+      { header: "Parol", key: "password", width: 18 },
+      { header: "Rol", key: "role", width: 15 },
+      { header: "Sinflar", key: "classes", width: 25 },
+    ],
+    data,
+  });
 
-    // Get class students
-    const students = await User.find({
-      classes: req.params.id,
-      role: "student",
-    })
-      .populate("classes", "name")
-      .select("+plainPassword")
-      .sort({ firstName: 1, lastName: 1 });
+  const filename = ExcelService.generateFileName(
+    `${classData.name}_oquvchilar`,
+  );
 
-    // Prepare data
-    const data = students.map((student) => ({
-      fullName: `${student.firstName} ${student.lastName || ""}`.trim(),
-      username: student.username,
-      password: student.plainPassword || "N/A",
-      role: "O'quvchi",
-      classes:
-        student.classes && student.classes.length > 0
-          ? student.classes.map((c) => c.name).join(", ")
-          : "-",
-    }));
-
-    // Create Excel with service
-    const workbook = ExcelService.createExcel({
-      sheetName: classData.name,
-      columns: [
-        { header: "F.I.O", key: "fullName", width: 30 },
-        { header: "Username", key: "username", width: 20 },
-        { header: "Parol", key: "password", width: 18 },
-        { header: "Rol", key: "role", width: 15 },
-        { header: "Sinflar", key: "classes", width: 25 },
-      ],
-      data,
-    });
-
-    // Generate filename
-    const filename = ExcelService.generateFileName(
-      `${classData.name}_oquvchilar`,
-    );
-
-    // Send file
-    await ExcelService.sendWorkbook(res, workbook, filename);
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  await ExcelService.sendWorkbook(res, workbook, filename);
+});
 
 // Export classes to Excel
-const exportClasses = async (req, res) => {
-  try {
-    const classes = await Class.find()
-      .populate("createdBy", "firstName lastName")
-      .sort({ name: 1 })
-      .lean();
+const exportClasses = asyncHandler(async (req, res) => {
+  const data = await classService.getAllClassesForExport();
 
-    const data = classes.map((classItem) => ({
-      name: classItem.name,
-      status: classItem.isActive ? "Faol" : "Faol emas",
-      createdBy: classItem.createdBy
-        ? `${classItem.createdBy.firstName} ${classItem.createdBy.lastName}`
-        : "-",
-      createdAt: new Date(classItem.createdAt).toLocaleDateString("uz-UZ"),
-    }));
+  const workbook = ExcelService.createExcel({
+    sheetName: "Sinflar",
+    columns: [
+      { header: "Sinf nomi", key: "name", width: 25 },
+      { header: "Holati", key: "status", width: 12 },
+      { header: "Yaratuvchi", key: "createdBy", width: 20 },
+      { header: "Yaratilgan sana", key: "createdAt", width: 15 },
+    ],
+    data,
+    headerStyle: {
+      bgColor: ExcelService.COLORS.HEADER_GREEN,
+    },
+  });
 
-    const workbook = ExcelService.createExcel({
-      sheetName: "Sinflar",
-      columns: [
-        { header: "Sinf nomi", key: "name", width: 25 },
-        { header: "Holati", key: "status", width: 12 },
-        { header: "Yaratuvchi", key: "createdBy", width: 20 },
-        { header: "Yaratilgan sana", key: "createdAt", width: 15 },
-      ],
-      data,
-      headerStyle: {
-        bgColor: ExcelService.COLORS.HEADER_GREEN,
-      },
-    });
-
-    const filename = ExcelService.generateFileName("sinflar");
-    await ExcelService.sendWorkbook(res, workbook, filename);
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  const filename = ExcelService.generateFileName("sinflar");
+  await ExcelService.sendWorkbook(res, workbook, filename);
+});
 
 module.exports = {
   getAllClasses,
--- src/routes/class.routes.js
+++ src/routes/class.routes.js
@@ -10,19 +10,21 @@ const {
   exportClasses,
 } = require("../controllers/class.controller");
 const { protect, authorize } = require("../middleware/auth.middleware");
+const { validateObjectId } = require("../middleware/validate.middleware");
+const { ROLES } = require("../utils/constants");
 
 // All routes are protected
 router.use(protect);
 
 // GET routes for everyone
 router.get("/", getAllClasses);
-router.get("/export", authorize("owner"), exportClasses);
-router.get("/:id", getClass);
-router.get("/:id/export", authorize("owner"), exportClassStudents);
+router.get("/export", authorize(ROLES.OWNER), exportClasses);
+router.get("/:id", validateObjectId("id"), getClass);
+router.get("/:id/export", validateObjectId("id"), authorize(ROLES.OWNER), exportClassStudents);
 
 // CUD operations for owner only
-router.post("/", authorize("owner"), createClass);
-router.put("/:id", authorize("owner"), updateClass);
-router.delete("/:id", authorize("owner"), deleteClass);
+router.post("/", authorize(ROLES.OWNER), createClass);
+router.put("/:id", validateObjectId("id"), authorize(ROLES.OWNER), updateClass);
+router.delete("/:id", validateObjectId("id"), authorize(ROLES.OWNER), deleteClass);
 
 module.exports = router;
