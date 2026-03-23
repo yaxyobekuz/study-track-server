f337064526f2a3206c8a6af309747504df80bc82
yaxyobekuz <pubgn9642@gmail.com>
2026-03-23 09:37:23 +0500

    feat: refactor auth controller to use asyncHandler for improved error handling

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,82 +1,25 @@
-const User = require("../models/user.model");
-const { generateToken } = require("../utils/jwt");
+const asyncHandler = require("../middleware/async.middleware");
+const authService = require("../services/auth.service");
 
 // Login
-const login = async (req, res) => {
-  try {
-    const { username, password } = req.body;
+const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  const data = await authService.login(username, password);
 
-    // Validation
-    if (!username || !password) {
-      return res.status(400).json({
-        success: false,
-        message: "Username va parol majburiy",
-      });
-    }
-
-    // Find user
-    const user = await User.findOne({
-      username: username.toLowerCase(),
-    }).populate("classes", "name");
-
-    if (!user || !(await user.matchPassword(password))) {
-      return res.status(400).json({
-        success: false,
-        message: "Username yoki parol noto'g'ri",
-      });
-    }
-
-    if (!user.isActive) {
-      return res.status(403).json({
-        success: false,
-        message: "Sizning hisobingiz faol emas",
-      });
-    }
-
-    // Generate token
-    const token = generateToken(user._id);
-
-    // Return response
-    res.json({
-      success: true,
-      data: {
-        user: {
-          id: user._id,
-          username: user.username,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          fullName: user.fullName,
-          role: user.role,
-          classes: user.classes,
-        },
-        token,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.json({
+    success: true,
+    data,
+  });
+});
 
 // Get current user data
-const getMe = async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id).populate("classes", "name");
+const getMe = asyncHandler(async (req, res) => {
+  const user = await authService.getMe(req.user._id);
 
-    res.json({
-      success: true,
-      data: user,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server xatosi",
-      error: error.message,
-    });
-  }
-};
+  res.json({
+    success: true,
+    data: user,
+  });
+});
 
 module.exports = { login, getMe };
