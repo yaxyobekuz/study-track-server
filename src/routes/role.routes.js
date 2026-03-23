f19670225b98bd202ebf25b4a78d8746a825d316
yaxyobekuz <pubgn9642@gmail.com>
2026-03-23 09:38:14 +0500

    feat: refactor role controller and routes for improved code organization and error handling

--- src/routes/role.routes.js
+++ src/routes/role.routes.js
@@ -8,10 +8,12 @@ const {
   deleteRole,
 } = require("../controllers/role.controller");
 const { protect, authorize } = require("../middleware/auth.middleware");
+const { validateObjectId } = require("../middleware/validate.middleware");
+const { ROLES } = require("../utils/constants");
 
 // All routes are protected and owner only
 router.use(protect);
-router.use(authorize("owner"));
+router.use(authorize(ROLES.OWNER));
 
 // GET routes
 router.get("/", getAllRoles);
@@ -19,7 +21,7 @@ router.get("/options", getRoleOptions);
 
 // CUD operations
 router.post("/", createRole);
-router.put("/:id", updateRole);
-router.delete("/:id", deleteRole);
+router.put("/:id", validateObjectId("id"), updateRole);
+router.delete("/:id", validateObjectId("id"), deleteRole);
 
 module.exports = router;
